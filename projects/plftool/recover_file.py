1fb492523f04ff71d6fd34a5e95329c12a928057
Raphaël Vinot <raphael@vinot.info>
2015-11-04 01:54:59 +0100

    descr 2 bytes

--- projects/plftool/recover_file.py
+++ projects/plftool/recover_file.py
@@ -5,15 +5,30 @@ import os
 import re
 import glob
 
+'''
+binary stuff
+b'm\x81' -> lib (very few)
+b'\xa4\x81' -> txt files
+b'\xedA' -> directory
+b'\xed\x81' -> exe
+b'\xff\xa1' -> link
+
+b'\x6d\x81' -> lib (very few)   555
+b'\xa4\x81' -> txt files        644
+b'\xed\x41' -> directory        755
+b'\xed\x81' -> exe              775
+b'\xff\xa1' -> symbolic links   (path in the file)
+'''
+
 for fn in sorted(glob.glob('a/*')):
-    print(fn)
     if fn == 'a/000_0x0b_0_volume_config' or fn == 'a/001_0x03_0_main_boot.plf':
         continue
     with open(fn, 'rb') as f:
         a, b, c = re.findall(br'([^\x00]*)\x00([^\x00]*)\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00(.*)', f.read(), re.DOTALL)[0]
-        if len(c) == 0:
+        print(a, b, len(c))
+        #if len(c) == 0:
             # Directory
-            os.mkdir('fs/' + a.decode("utf-8"))
-        else:
-            with open('fs/' + a.decode("utf-8"), 'wb') as nf:
-                nf.write(c)
+            #os.mkdir('fs/' + a.decode("utf-8"))
+        #else:
+            #with open('fs/' + a.decode("utf-8"), 'wb') as nf:
+            #    nf.write(c)
